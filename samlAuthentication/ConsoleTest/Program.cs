8a354572f4e388880183a5f9f4eff31906aa6a34
Ayyaz MAHMOOD PARACHA <a.mahmood@esri.com>
2022-06-23 17:44:11 +0200

    Initial Test SAML Authentication

---
+++ samlAuthentication/ConsoleTest/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static string ProcessInput(string s)
+        {
+            // TODO Verify and validate the input 
+            // string as appropriate for your application.
+            return s;
+        }
+        static void Main(string[] args)
+        {
+            //https://github.com/googlesamples/oauth-apps-for-windows/tree/master/OAuthConsoleApp
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Alert.exe invoked with the following parameters.\r\n");
+            //Console.WriteLine("Raw command-line: \n\t" + Environment.CommandLine);
+
+            //Console.WriteLine("\n\nArguments:\n");
+            //foreach (string s in args)
+            //{
+            //    Console.WriteLine("\t" + ProcessInput(s));
+            //}
+            //Console.WriteLine("\nPress any key to continue...");
+            //Console.ReadKey();
+
+
+            //Pass the filepath and filename to the StreamWriter Constructor
+            //StreamWriter sw = new StreamWriter("C:\\data\\test\\Test.txt");
+            ////Write a line of text
+            //sw.WriteLine("Hello World!!");
+            ////Write a second line of text
+            //sw.WriteLine("From the StreamWriter class");
+            //sw.WriteLine("Alert.exe invoked with the following parameters.\r\n");
+            //sw.WriteLine("Raw command-line: \n\t" + Environment.CommandLine);
+            //string CommandLine = Environment.CommandLine;
+            //string access_token = CommandLine.Split("#")[1].Split("=")[1].Split("&")[0];
+            //sw.WriteLine("Access Token: \n\t" + access_token);
+            ////Close the file
+            //sw.Close();
+
+            Console.WriteLine("Hello World!");
+            //using System.Diagnostics.Process cmd = new();
+            //Process cmd = new Process();
+            //cmd.Start();
+            //cmd.StartInfo.FileName = "explorer.exe";
+
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+
+
+            cmd.StandardInput.WriteLine("start https://google.com");
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+            
+
+            //cmd.Start("explorer.exe", "http://google.com");
+            //System.Diagnostics.Process.Start("explorer.exe", "http://google.com");
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ samlAuthentication/samlAuthentication/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace samlAuthentication
+{
+    public partial class Form1 : Form
+    {
+        string AccessToken = "";
+        public Form1(string v)
+        {
+            InitializeComponent();
+            AccessToken = v;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.radioButton1.Checked == true)
+            {
+                this.textBox3.Visible = true;
+            }
+            else
+            {
+                this.textBox3.Visible = false;
+            }
+            if (this.radioButton2.Checked == true)
+            {
+                this.pictureBox1.Visible = true;
+            }
+            else
+            {
+                this.pictureBox1.Visible = false;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.radioButton1.Checked == true)
+            {
+                this.textBox3.Visible = true;
+            }
+            else
+            {
+                this.textBox3.Visible = false;
+            }
+            if (this.radioButton2.Checked == true)
+            {
+                this.pictureBox1.Visible = true;
+            }
+            else
+            {
+                this.pictureBox1.Visible = false;
+            }
+        }
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    string url = "https://ps0014727.esri.com/portal/sharing/rest/oauth2/authorize?client_id=qhCTGOZoyTePbYC9&expiration=20160&redirect_uri=urn:ietf:wg:oauth:2.0:oob&response_type=token&locale=en-US";
+        //    var info = new ProcessStartInfo(url);
+
+        //    string ProgramPath = "C:/Program Files/Google/Chrome/Application/chrome.exe";
+
+        //    Process.Start(ProgramPath, url);
+        //}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string access_token = AccessToken.Split("#")[1].Split("=")[1].Split("&")[0];
+            this.textBox4.Text = access_token;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string TestUrl = this.textBox2.Text;
+            TestUrl = TestUrl + "&token=" + this.textBox4.Text;
+            if (this.radioButton1.Checked == true)
+            {
+                using (var client = new WebClient())
+                {
+                    var responseString = client.DownloadString(TestUrl);
+                    string JsonResponse = responseString.ToString();
+                    this.textBox3.Text = JsonResponse;
+                }
+            }
+            else
+            {
+                using (System.Net.WebClient webClient = new System.Net.WebClient())
+                {
+                    using (Stream stream = webClient.OpenRead(TestUrl))
+                    {
+                        this.pictureBox1.Image = Image.FromStream(stream);
+                    }
+                }
+            }
+
+
+        }
+    }
+}
